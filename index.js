ce9f193589e6eb245ee42f7f81c02805febbc996
Abdullah Naifeh <abdna59@gmail.com>
2022-10-14 15:10:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+// for (let i = 0; i < gifts.length ; i++){
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`)
+// };
+// return gifts;
+// }
+// wrapGifts(gifts)
+
+
+
+ function writeCards(arr, eventname) {
+     let newArr = [];
+     for (let i = 0; i < arr.length ; i++){
+         newArr.push("Thank you, "+arr[i]+", for the wonderful " + eventname + " gift!")
+     } return newArr ;  
+ }
+
+ console.log(writeCards(["Charlie", "Samip", "Ali"], "surprise"));
+
+
+function countDown (number){
+    let num = 0;
+    while( number >= num){
+    console.log(num++)
+    }
+    return number;
+}
+countDown(10)
